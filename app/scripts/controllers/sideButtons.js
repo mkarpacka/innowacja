44cb9d1ab92ded9f0732d6abbd66f472562feecf
AIwinski <artur.iwinski@gmail.com>
2018-06-05 09:40:18 +0200

    configured urls in side buttons

--- app/scripts/controllers/sideButtons.js
+++ app/scripts/controllers/sideButtons.js
@@ -1,7 +1,26 @@
 app.controller('SideButtonsCtrl', function($scope, $rootScope, $location){
     $rootScope.$on('$routeChangeStart', function(){
         switch($location.path()){
-            //case: '/' //skoncze to jutro prosze tego nie ruszac :)
+            case '/':
+                $scope.nextUrl = '#/about';
+                $scope.prevUrl = '#/restrictions';
+                break;
+            case '/about':
+                $scope.nextUrl = '#/authorization';
+                $scope.prevUrl = '#/';
+                break;
+            case '/authorization':
+                $scope.nextUrl = '#/basics';
+                $scope.prevUrl = '#/about';
+                break;
+            case '/basics':
+                $scope.nextUrl = '#/blockchain';
+                $scope.prevUrl = '#/authorization';
+                break;
+            case '/blockchain':
+                $scope.nextUrl = '#/';
+                $scope.prevUrl = '#/basics';
+                break;
         }
     });
 });
