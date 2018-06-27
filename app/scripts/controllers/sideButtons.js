df3056e55fdeaeb7881ddcc0e92d6e58f0b021f9
AIwinski <artur.iwinski@gmail.com>
2018-06-27 10:42:19 +0200

    added some animations

--- app/scripts/controllers/sideButtons.js
+++ app/scripts/controllers/sideButtons.js
@@ -1,28 +1,63 @@
-app.controller('SideButtonsCtrl', function($scope, $rootScope, $location){
+app.controller('SideButtonsCtrl', function ($scope, $rootScope, $location) {
 
-    
-    $rootScope.$on('$routeChangeStart', function(){
-        switch($location.path()){
-            case '/':
-                $scope.nextUrl = '#/about';
-                $scope.prevUrl = '#/restrictions';
-                break;
-            case '/about':
-                $scope.nextUrl = '#/authorization';
-                $scope.prevUrl = '#/';
-                break;
-            case '/authorization':
-                $scope.nextUrl = '#/basics';
-                $scope.prevUrl = '#/about';
-                break;
-            case '/basics':
-                $scope.nextUrl = '#/blockchain';
-                $scope.prevUrl = '#/authorization';
-                break;
-            case '/blockchain':
-                $scope.nextUrl = '#/';
-                $scope.prevUrl = '#/basics';
-                break;
-        }
-    });
+
+  var side = 'left';
+
+  $scope.getTransitionInclude = function () {
+    if (side === 'left') {
+      return '../styles/slideLeft.css';
+    } else {
+      return '../styles/slideRight.css';
+    }
+  }
+
+  $scope.setLeftSide = function () {
+    side = 'left';
+  }
+
+  $scope.setRightSide = function () {
+    side = 'right';
+  }
+
+
+  $rootScope.$on('$routeChangeStart', function () {
+    switch ($location.path()) {
+      case '/':
+        $scope.nextUrl = '#/about';
+        $scope.prevUrl = '#/restrictions';
+        break;
+      case '/about':
+        $scope.nextUrl = '#/authorization';
+        $scope.prevUrl = '#/';
+        break;
+      case '/authorization':
+        $scope.nextUrl = '#/demo';
+        $scope.prevUrl = '#/about';
+        break;
+      case '/demo':
+        $scope.nextUrl = '#/comparing';
+        $scope.prevUrl = '#/authorization';
+        break;
+      case '/comparing':
+        $scope.nextUrl = '#/hashPuzzle';
+        $scope.prevUrl = '#/demo';
+        break;
+      case '/hashPuzzle':
+        $scope.nextUrl = '#/hashing';
+        $scope.prevUrl = '#/comparing';
+        break;
+      case '/hashing':
+        $scope.nextUrl = '#/payingForIntegrity';
+        $scope.prevUrl = '#/hashPuzzle';
+        break;
+      case '/payingForIntegrity':
+        $scope.nextUrl = '#/restrictions';
+        $scope.prevUrl = '#/hashPuzzle';
+        break;
+      case '/restrictions':
+        $scope.nextUrl = '#/';
+        $scope.prevUrl = '#/payingForIntegrity';
+        break;
+    }
+  });
 });
