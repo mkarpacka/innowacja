c1b96509ca91e09c0bf1623911289516d2ab4cb8
AIwinski <artur.iwinski@gmail.com>
2018-06-28 11:18:36 +0200

    fajny nawbar

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -1,27 +1,41 @@
-jQuery(document).ready(function ($) {
-  var MQL = 1170;
-  //primary navigation slide-in effect
-  if ($(window).width() > MQL) {
-    var headerHeight = $('.navbar-custom').height();
-    $(window).on('scroll', {
-        previousTop: 0
-      },
-      function () {
-        var currentTop = $(window).scrollTop();
-        //check if user is scrolling up
-        if (currentTop < this.previousTop) {
-          //if scrolling up...
-          if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-            $('.navbar-custom').addClass('is-visible');
-          } else {
-            $('.navbar-custom').removeClass('is-visible is-fixed');
-          }
-        } else if (currentTop > this.previousTop) {
-          //if scrolling down...
-          $('.navbar-custom').removeClass('is-visible');
-          if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-        }
-        this.previousTop = currentTop;
-      });
+// var sscrl = new SmoothScroll('a[href*="#"]');
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+  scroll()
+};
+window.onload = function () {
+  scroll();
+}
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scroll() {
+  if (!(window.pageYOffset > sticky)) {
+    navbar.classList.add("stick");
+  } else {
+    navbar.classList.remove("stick");
   }
-});
+}
+
+function isElementInViewport(el) {
+
+  //special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+  );
+}
